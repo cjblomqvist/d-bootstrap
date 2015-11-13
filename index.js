366543d74478427237d4be7d712ca12c14f85719
cjblomqvist <carl@cjblomqvist.com>
2015-11-13 21:13:39 +0100

    Fix compatibility with npm 3.x/flat node_modules

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = function(app, options) {
   app.component(require('./tabs'));
   app.component(require('./alert'));
   app.component(require('./contextMenu'));
-  if(!options || (options && options.loadStyles)) app.loadStyles(__dirname + '/node_modules/bootstrap/dist/css/bootstrap.min');
+  if(!options || (options && options.loadStyles)) app.loadStyles(__dirname + '/../bootstrap/dist/css/bootstrap.min');
 };
